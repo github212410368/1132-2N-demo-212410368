d44d0e3cd86ba28957c5a84165a6286a389606b1
ting <212410368@o365.tku.edu.tw>
2025-02-19 21:12:23 +0800

    W01-P3: Show Navbar_xx responsive from links data

---
+++ demo/w01_tailwind_68/src/App_68.jsx
@@ -0,0 +1,11 @@
+import Navbar_68 from './components/Navbar_68';
+
+const App_68 = () => {
+  return (
+    <>
+      <Navbar_68 />
+    </>
+  );
+};
+
+export default App_68;
---
+++ demo/w01_tailwind_68/src/components/Projects_68.jsx
@@ -0,0 +1,8 @@
+const Projects_68 = () => {
+  return (
+    <section>
+      <h1>Project_68</h1>
+    </section>
+  );
+};
+export default Projects_68;
