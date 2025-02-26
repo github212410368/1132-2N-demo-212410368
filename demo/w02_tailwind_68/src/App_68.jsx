87fc66f3f27e724da798097747f86df83f5a3441
ting <212410368@o365.tku.edu.tw>
2025-02-26 19:38:41 +0800

    W02-P1: Show <Hero_xx > with screen width = 768px

---
+++ demo/w02_tailwind_68/src/App_68.jsx
@@ -0,0 +1,12 @@
+import Navbar_68 from './components/Navbar_68';
+import Hero_68 from './components/Hero_68';
+function App_68() {
+  return (
+    <>
+      <Navbar_68 />
+      <Hero_68 />
+    </>
+  );
+}
+
+export default App_68;
---
+++ demo/w02_tailwind_68/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index_68.css';
+import App_68 from './App_68.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App_68 />
+  </StrictMode>
+);
