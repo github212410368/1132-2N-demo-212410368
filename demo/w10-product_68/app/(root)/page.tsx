9060d24f6ec4714b4e22e5e791670da359c763e2
ting <212410368@o365.tku.edu.tw>
2025-04-30 18:27:51 +0800

    W10-P1: Do p4_xx in midterm

---
+++ demo/w10-product_68/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+const HomePage_68 = () => {
+  return (
+    <div>
+      <Button variant={'outline'}>lcq</Button>
+      <Button variant={'default'}>lcq</Button>
+    </div>
+  );
+};
+export default HomePage_68;
